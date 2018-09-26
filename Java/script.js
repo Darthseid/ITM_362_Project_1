3141fe4e925eeade49857c544838cb34428184e3
Darthseid <jared.mcvey@yahoo.com>
2018-09-26 09:58:49 -0500

    Got rid of button

--- Java/script.js
+++ Java/script.js
@@ -1,44 +1,23 @@
-
-  function confirmSubmit(evt)
-  {
-  var fullname = document.getElementById("firstname");
-  var button = document.getElementById('submission');
-
-  button.onclick = function()
-  {
-    style.display = "block";
-  }
-  alert("Thanks " + firstname.value + " for joining us");
-  }
+var slide_number = 0; 
+ function slidechange()
+{  
+        var i;
+    var slides = document.getElementsByClassName("sliding");
+    var circle = document.getElementsByClassName("circles");
+    for (i = 0; i < slides.length; i++) 
+    {
+       slides[i].style.display = "none";  
+    }
+    slide_number++;
+    if (slide_number > slides.length) {slide_number = 1}    
+    for (i = 0; i < circle.length; i++) 
+    {
+        circle[i].className = circle[i].className.replace(" activated", "");
+    }
+    slides[slide_number-1].style.display = "block";  
+    circle[slide_number-1].className += " activated";
+    setTimeout(slidechange, 2100); 
+}
   
-  var Indexed = 1;
-showSlides(slideIndex);
 
-function changeSlides(n) 
-{
-  showSlides(slideIndex += n);
-}
 
-function presentSlide(n) 
-{
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n)
- {
-  var i;
-  var slide = document.getElementsByClassName("gallery");
-  var dots = document.getElementsByClassName("circle");
-  if (n > slide.length) {Indexed = 1} 
-  if (n < 1) {Indexed = slide.length}
-  for (i = 0; i < slide.length; i++) 
-  {
-      slide[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++)
-	  {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slide[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
