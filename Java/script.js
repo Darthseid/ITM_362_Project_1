04392bf38b7b15d87fde6e6f728e99fff2a64b4b
Darthseid <jared.mcvey@yahoo.com>
2018-09-30 01:36:34 -0500

    Changed slideshow to jquery code

--- Java/script.js
+++ Java/script.js
@@ -1,23 +1,11 @@
-var slide_number = 0; 
- function slidechange()
-{  
-        var i;
-    var slides = document.getElementsByClassName("sliding");
-    var circle = document.getElementsByClassName("circles");
-    for (i = 0; i < slides.length; i++) 
-    {
-       slides[i].style.display = "none";  
-    }
-    slide_number++;
-    if (slide_number > slides.length) {slide_number = 1}    
-    for (i = 0; i < circle.length; i++) 
-    {
-        circle[i].className = circle[i].className.replace(" activated", "");
-    }
-    slides[slide_number-1].style.display = "block";  
-    circle[slide_number-1].className += " activated";
-    setTimeout(slidechange, 2100); 
-}
-  
+$("#slideshow > div:gt(0)").hide();
 
-
+setInterval(function() 
+{ 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
