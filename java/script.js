3f6447624cee788fb60b35cec59b644b83fd9251
Darthseid <jared.mcvey@yahoo.com>
2018-10-25 17:30:18 -0500

    Create script.js

---
+++ java/script.js
@@ -0,0 +1,17 @@
+jQuery(function($) 
+{
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() 
+{ 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
